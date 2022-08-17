830720047e21d8543b7151b63f906725261839a7
Matias Diaz <Matiascdiaz@gmail.com>
2022-08-17 01:41:02 -0300

    aplicando fetch

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
-
-let listaJSON = '[{"ciudad":"moreno","temMax":"34","temMin":"15","hum":"90"},{"ciudad":"paso del rey","temMax":"34","temMin":"17","hum":"91"},{"ciudad":"merlo","temMax":"35","temMin":"18","hum":"93"},{"ciudad":"ituzaingo","temMax":"35","temMin":"18","hum":"91"},{"ciudad":"moron","temMax":"38","temMin":"20","hum":"90"},{"ciudad":"ramos mejia","temMax":"38","temMin":"23","hum":"93"},{"ciudad":"ciudadela","temMax":"40","temMin":"24","hum":"95"},{"ciudad":"liniers","temMax":"41","temMin":"24","hum":"96"},{"ciudad":"villa luro","temMax":"39","temMin":"23","hum":"97"},{"ciudad":"caballito","temMax":"40","temMin":"23","hum":"98"},{"ciudad":"caba","temMax":"42","temMin":"25","hum":"99"}]';
-
-
-let listaCiudades = JSON.parse(listaJSON);
-
-
 // ---- FUNCIONES------//
 
-function listarCiudades(){ 
-    return listaCiudades.map((c,i) => `${i}-${c.ciudad} \n`).reduce((acumulador,elemento) => acumulador.concat(elemento),"")
-} 
-
-
 function buscarCiudad(ciudad){
-    return listaCiudades.find((info) => info.ciudad === ciudad);
+    return fetch(`http://api.weatherapi.com/v1/current.json?key=0b0585a3e5b643db821230808221508&q=${ciudad}`)
+    .then(response => response.json())
+    .then(result => {return {
+        ciudad: `${result.location.name}, ${result.location.region}`,
+        temp: `${result.current.temp_c}`,
+        sensacionTer: `${result.current.feelslike_c}`,
+        hum: `${result.current.humidity}`
+    }})
 }  
 
+let input = document.getElementById("input"); 
 
 function pedirCiudad(){ 
-    let input = document.getElementById("input");
     return input.value !== "" ? input.value.toLowerCase() : mostrarToast(`INGRESE UNA CIUDAD VALIDA`); 
 } 
 
@@ -27,15 +22,16 @@ function mostrarHTML (ciudad){
     let ubicacion = document.getElementById("ubi");
     ubicacion.innerHTML= ciudad.ciudad;
 
-    let tempMax = document.getElementById("temMax");
-    tempMax.innerHTML = ciudad.temMax;
+    let tempMax = document.getElementById("temp");
+    tempMax.innerHTML = ciudad.temp;
     
-    let tempMin = document.getElementById("temMin");
-    tempMin.innerHTML = ciudad.temMin;
+    let tempMin = document.getElementById("sensacionTer");
+    tempMin.innerHTML = ciudad.sensacionTer;
 
     let humedad = document.getElementById("hum");
     humedad.innerHTML = ciudad.hum;
 }
+
 
 
 function cambiarPantalla(){
@@ -43,7 +39,7 @@ function cambiarPantalla(){
     wrapper.classList.toggle("active");
 } 
 
-function mostrarToast(texto){ //creamos la función que nos permita mostrar el toastify, ya que de esta manera si queremos modificar algo de la misma, lo haremos dentro de la función y se aplicará en cualquier lado que la llamemos. 
+function mostrarToast(texto){ 
     Toastify({
         text: texto,
         gravity: "top",
@@ -51,6 +47,7 @@ function mostrarToast(texto){ //creamos la función que nos permita mostrar el t
         backgroundColor: "linear-gradient(to right, #ff4137, #ff4137)",
       }).showToast();
 }
+
  
 let button = document.getElementById("button");
 button.addEventListener("click", (e)=>{
@@ -58,13 +55,13 @@ button.addEventListener("click", (e)=>{
         if (resultado == undefined){
             return
         } else {
-            let ciudad = buscarCiudad(resultado);
-            if (ciudad == undefined){
-                mostrarToast("NO SE ENCONTRÓ LA CIUDAD");
-            } else{
+            buscarCiudad(resultado)
+            .then(ciudad => {
                 mostrarHTML(ciudad);
                 cambiarPantalla();
-            }
+            })
+
+            .catch( p => mostrarToast("NO SE ENCONTRÓ LA CIUDAD"));
         }
     })
 
@@ -72,4 +69,23 @@ let arrow = document.getElementById("arrow");
 arrow.addEventListener("click", cambiarPantalla);
 
 
+input.addEventListener("keyup", (e)=>{
 
+    let target = e.target
+    let cantidad = target.value.length;
+    let sug = document.getElementById("sugerencias");
+    if ( cantidad >= 3){
+       fetch(`https://api.weatherapi.com/v1/search.json?key=0b0585a3e5b643db821230808221508&q=${e.target.value}`)
+       .then(response => response.json())
+       .then(result => {
+        sug.innerHTML="";
+        result.forEach(element => {
+            sug.innerHTML += `
+                <option value = "${element.name},${element.region},${element.country}"> 
+            `
+        });   
+    });
+    } else {
+        sug.innerHTML = "";
+    }
+})
